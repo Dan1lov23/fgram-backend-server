79e12ed133a4d23ce83e715ff5dca79cbb3f9f00
kitik <svy1dan@gmail.com>
2026-02-09 14:26:12 +0300

    messages db create config

---
+++ db create config/messages db create config/messagesDbCreateConfig.js
@@ -0,0 +1,18 @@
+const Database = require('better-sqlite3');
+
+const messagesDb = new Database('../databases/messages db/messages.db');
+
+try {
+    messagesDb.exec(`CREATE TABLE IF NOT EXISTS messages (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        sender TEXT,
+        recipient TEXT,
+        messageText TEXT,
+        messageTime INTEGER
+    )`);
+    // console.log('Message db connection is success.');
+} catch (err) {
+    console.error('Db open error: ' + err.message);
+}
+
+module.exports = messagesDb;
